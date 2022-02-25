f80f337311124dc3d09fcbffa9ccd198a588f08c
Kreativan <kreativan.dev@gmail.com>
2022-02-25 11:13:14 +0200

    11ty images, navigation, filters...

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const { DateTime } = require("luxon");
 
 //
@@ -30,26 +31,66 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/");
   eleventyConfig.addWatchTarget("./src/assets/");
 
-  // {% image "./src/images/cat.jpg", "my photo" %}
-  // {% image "./src/images/cat.jpg", "my photo", "(min-width: 30em) 50vw, 100vw" %}
+  /**
+   * Eleventy Navigation
+   * @example
+   * {% set navPages = collections.all | eleventyNavigation %}
+   * {% for item in navPages %}...{% endfor %}
+   */
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  /**
+   * Eleventy Image 
+   * @examples
+   * {% image "./src/images/cat.jpg", "my photo" %}
+   * {% image "./src/images/cat.jpg", "my photo", "(min-width: 30em) 50vw, 100vw" %}
+   */
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
   eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
-  // css, js files cache buster timestamp
-  // {{ '/assets/script.js' | buster }}
+  /**
+   * Buster
+   * Add timestamp at the end of the filename.
+   * Used to bust browser cache for css and js files
+   * @example {{ '/assets/script.js' | buster }}
+   */
   eleventyConfig.addFilter("buster", function(value) {
     return value + "?" + Date.now();
   });
 
-  /* Luxon Date Filter {{ item.date | luxonDate }} */
+  /**
+   * Get the base url eg: /blog/post/ => /blog/
+   * @example {{ page.url | baseUrl }}
+   * @use Used this to add active classes on "parent" element
+   * @see navbar.njk
+   */
+  eleventyConfig.addFilter("baseUrl", function(value) {
+    if(!typeof value === "string") return;
+    let array = value.split("/");
+    if(!array[2]) return value;
+    let baseURL = array[1];
+    return `/${baseURL}/`;
+  });
+
+  /** 
+   *  Luxon Date Filter
+   *  @example {{ item.date | luxonDate }}
+   */
   eleventyConfig.addFilter("luxonDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
   });
 
-  /* Year Shortcode {% year %} */
+  /**
+   * Year Shortcode
+   * @example {% year %}
+   */
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
-  // Get only content that matches a tag 
+  /**
+   *  Featured blog posts - Custom Collection
+   *  Get only content that matches a blog and featured tag 
+   *  @example {% for post in blogFeatured.collections  %}...{% endif %}
+   */
   eleventyConfig.addCollection("blogFeatured", function(collectionApi) {
     return collectionApi.getFilteredByTags('blog', 'featured');
   });
--- src/pages/page-two.md
+++ src/pages/page-two.md
@@ -1,5 +1,7 @@
 ---
 title: Page Two
+eleventyNavigation:
+  key: Page Two
 ---
 
 Qui enim voluptate ut pariatur occaecat irure velit ad proident adipisicing Lorem adipisicing. Consequat pariatur veniam duis nisi voluptate non laborum nisi cillum sit nostrud proident mollit non. Tempor consectetur esse ut et consequat velit quis pariatur elit ullamco.
