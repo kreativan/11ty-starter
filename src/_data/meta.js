a0eb2ef9ed7903bc4b9500737fc0d1d9d24a8088
Kreativan <kreativan.dev@gmail.com>
2022-03-07 13:39:44 +0200

    update

---
+++ src/_data/meta.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "kreativan.dev",
+  description: "Web Development",
+  website: "www.kreativan.dev",
+  author: "Ivan Milincic",
+  github: "https://github.com/kreativan", // no `@`
+};
